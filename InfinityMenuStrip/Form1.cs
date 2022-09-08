4cd20c37d4416913df87e5a5359426674566b9eb
zuzeyka <stepan.tarasenko.26@gmail.com>
2022-09-08 14:51:07 +0300

    Добавьте файлы проекта.

---
+++ InfinityMenuStrip/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfinityMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        MenuStrip menuStrip1;
+        int counter = 1;
+        ToolStripMenuItem parent;
+        public Form1()
+        {
+            InitializeComponent();
+            menuStrip1 = new MenuStrip();
+            this.Controls.Add(menuStrip1);
+            menuStrip1.Items.Add(parent = new ToolStripMenuItem(counter.ToString()));
+            parent.MouseHover += ItemMouseHover;
+        }
+
+        private void ItemMouseHover(object sender, EventArgs e)
+        {
+            parent.DropDownItems.Add(parent = new ToolStripMenuItem(counter.ToString()));
+            parent.MouseHover += ItemMouseHover;
+            counter++;
+        }
+    }
+}
